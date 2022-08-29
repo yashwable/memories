47be173b9bfeff1b02b9dac6ceb5d8bfef4f9874
yash <yashwable12@gmail.com>
2022-08-29 22:25:47 +0530

    fourth day

--- server/index.js
+++ server/index.js
@@ -9,11 +9,11 @@ app.use(bodyParser.json({ limit : "30mb" , extended : true}));
 app.use(bodyParser.urlencoded ({ limit : "30mb" , extended : true}));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://memories:<password>@cluster0.l5hoi47.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://memories:memories@cluster0.l5hoi47.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL,{ useNewUrlParser: true , useUnifiedTopology: true})
-.then( () => app.listen(PORT, () => console.log('server running on port :${PORT}')))
+.then(() => app.listen(PORT, () => console.log('server running on port : ' + PORT)))
 .catch((error) => console.log(error.message));
 
-mongoose.set('useFindAndModify',false);
+// mongoose.set('useFindAndModify',false);
