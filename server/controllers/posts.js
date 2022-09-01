9cfa9af3d761a35ffcd0e83fb111e4b0283ece7f
yash <yashwable12@gmail.com>
2022-09-01 19:18:37 +0530

    sixth day

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,24 @@
-export const getPosts = (req,res) => {
-    res.send('it works')
+import PostMessage from '../models/postMessage.js'
+// const PostMessage = require('../models/postMessage');
+
+export const getPosts = async (req,res) => {
+    try {
+        
+        const PostMessages = await PostMessage.find();
+        res.status(200).json({PostMessages});
+    } catch (error) {
+        res.status(404).json({ message : error.message });
+    }
+}
+
+export const createPosts = async (req,res) => {
+    const post = req.body;
+    const newPost = new PostMessage(post);
+
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message : error.message });
+    }
 }
