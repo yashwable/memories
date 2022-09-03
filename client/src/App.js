e4a674a882b7414ace7c80d4ad58894c167cd849
yash <yashwable12@gmail.com>
2022-09-03 20:00:56 +0530

    eighth day

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import {Container , Appbar , Typography ,Grid ,Grow} from '@material-ui/core';
+import {Container , AppBar , Typography ,Grid ,Grow} from '@material-ui/core'; 
+import memories from './images/memories.png';
+import Form from './components/Form/Form.js';
+import Posts from './components/Posts/Posts.js';
+
 
 const App = () => {
     return(
-        <div>
-            <h1>This is our app</h1>
-        </div>
+        <Container maxWidth = "lg">
+            <AppBar position ="static" color="inherit">
+                <Typography varient = "h2" align='center'>memories</Typography>
+                <img src={memories} alt='memories' height = "60" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems='streach' spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts/>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form/>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 }
 
