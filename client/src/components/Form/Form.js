3fc3782b0acfec4e7c5e7e20002c424ea5263610
yash <yashwable12@gmail.com>
2022-09-06 22:48:59 +0530

    twelwth day

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,5 +1,6 @@
 import React,{useState} from "react";
 import { TextField,Button,Typography,Paper } from "@material-ui/core";
+import FileBase from 'react-file-base64';
 import useStyles from './styles';
 
 const Form = () => {
@@ -11,6 +12,9 @@ const Form = () => {
     const handleSubmit = () => {
 
     }
+    const clear = () => {
+
+    }
     return (
         <Paper className={classes.paper}>
             <Form autoComplete = "off" noValidate className = {classes.form} onSubmit ={handleSubmit}>
@@ -19,6 +23,10 @@ const Form = () => {
                 <TextField name="title" variant="outlined" label = "Title" fullWidth value={postData.title} onChange = { (e) => setPostData({...postData, title:e.target.value}) } />
                 <TextField name="message" variant="outlined" label = "Message" fullWidth value={postData.message} onChange = { (e) => setPostData({...postData, message:e.target.value}) } />
                 <TextField name="tags" variant="outlined" label = "Tags" fullWidth value={postData.tags} onChange = { (e) => setPostData({...postData, tags:e.target.value}) } />
+                <div className={classes.fileInput}> <FileBase type = "file" multiple = {false}  onDone = {({base64}) => postData({...postData,selectedFile:base64 })}/> </div>
+                <Button className={classes.buttonSubmit} variant = "container" color = "primary" size = "large" type="submit" fullWidth>Submit</Button>
+                <Button variant = "contained" color = "secondary" size = "small" onClick={clear} fullWidth>clear</Button>
+
             </Form>
         </Paper>
     );
